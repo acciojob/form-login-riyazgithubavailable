91faba7cf35338bb0a79982f43f3ea8c0fe004c8
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-08-17 15:39:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById("firstName").value;
+	const secondName = document.getElementById("secondName").value;
+	alert(firstName +" "+secondName)
 
 }
